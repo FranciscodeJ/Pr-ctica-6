332bae928f7fc2c3248effd9c238efe77c18f334
FranciscodeJ <147544324+FranciscodeJ@users.noreply.github.com>
2023-10-20 16:18:27 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -24,11 +24,11 @@ namespace Práctica__6
 
         private void button1_Click(object sender, EventArgs e)
         {
-            float valor1 = float.Parse(textBox1.Text);
+            float valor1 = float.Parse(textBox1.Text);       // Se declaran los espacios en las cajas de texto para los valores que se escribirán en la base y en la altura del tríangulo.
             float valor2 = float.Parse(textBox2.Text);
-            float valor3 = valor1 * valor2 / 2;
+            float valor3 = valor1 * valor2 / 2;                //Se declará la fórmula para obtener el área  del triángulo.
 
-            textBox3.Text = valor3.ToString();
+            textBox3.Text = valor3.ToString();                 //Se declara que en la carja de texto 3 se mostrará el valor resultante de la fórmula del área del tríangulo.
 
         }
     }
